9f4d6fb2ea713cba529b0461bd5555882b9989a8
bun349 <bunga05adlyna@gmail.com>
2024-09-11 00:44:55 +0700

    update folder

--- Pemrograman-Web/Tugas-3-Part-2/Tugas3_Part2script.js
+++ Pemrograman-Web/Tugas-3-Part-2/Tugas3_Part2script.js
@@ -1,24 +1,28 @@
-
 var val1, val2;
 
 function konversi() {
     val1 = parseInt(document.kirim.T1.value) || 0; 
     val2 = parseInt(document.kirim.T2.value) || 0;
+    console.log('val1:', val1, 'val2:', val2); // Debugging
 }
 
 function tambah() {
     konversi();
-    document.kirim.T3.value = val1 + val2; 
+    document.kirim.T3.value = val1 + val2;
 }
+
 function kurang() {
-    konversi(); 
-    document.kirim.T3.value = val1 - val2; 
+    konversi();
+    document.kirim.T3.value = val1 - val2;
 }
+
 function bagi() {
-    konversi(); 
-    document.kirim.T3.value = val1 / val2; 
+    konversi();
+    document.kirim.T3.value = val1 / val2;
 }
+
 function kali() {
-    konversi(); 
-    document.kirim.T3.value = val1 * val2; 
+    konversi();
+    document.kirim.T3.value = val1 * val2;
+    console.log('Hasil kali:', document.kirim.T3.value); 
 }
